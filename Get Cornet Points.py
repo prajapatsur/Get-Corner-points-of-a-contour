77fe902eba91600477be52295cf2dc11a9269942
Surendra Prajapat <100709956+prajapatsur@users.noreply.github.com>
2022-08-19 11:47:58 +0530

    Update Get Cornet Points.py

--- Get Cornet Points.py
+++ Get Cornet Points.py
@@ -11,7 +11,7 @@ img_copy = img.copy()
 #detecting contours
 contours,heirarchy = cv2.findContours(img_canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-#function to get corner points of any contour
+#Defining function to get corner points of any contour
 def getCornerPoints(contour):
     peri = cv2.arcLength(contour,True)
     approx = cv2.approxPolyDP(contour, 0.02*peri,True)
@@ -20,6 +20,7 @@ def getCornerPoints(contour):
 #drawing contours
 cv2.drawContours(img,contours,-1,(0,255,0),3,cv2.LINE_AA)
 
+#function calling
 Corner_points = getCornerPoints(contours[0])
 print(Corner_points)
 
@@ -27,5 +28,3 @@ cv2.imshow('Contours',img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
